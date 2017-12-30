9bf2c02424b7d1cf76e17edd48df7a4aeb5ad71c
romain <romainlembo06@gmail.com>
2017-12-30 12:37:03 +0100

    change checking by guard

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,13 +1,22 @@
+/*
+ * TUTO : http://jasonwatmore.com/post/2016/08/16/angular-2-jwt-authentication-example-tutorial
+ */
+
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {DashboardComponent} from '../dashboard/dashboard.component';
-import {TestComponent} from '../test/test.component';
+import {LoginComponent} from '../login/login.component';
+import {GuardService} from '../../services/guard.service';
 
 const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'test', component: TestComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: DashboardComponent, canActivate: [GuardService] },
+    //{ path: 'dashboard', component: DashboardComponent },
+    
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {TestComponent} from './test/test.component';
+import {LoginComponent} from './login/login.component';
+
+import {AuthentificationService} from '../services/authentification.service';
+import {GuardService} from '../services/guard.service';
 
 @NgModule({
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        HttpModule
     ],
     declarations: [
         AppComponent,
-        TestComponent,
-        DashboardComponent
+        DashboardComponent,
+        LoginComponent
     ],
-    providers: [],
+    providers: [
+        AuthentificationService,
+        GuardService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
